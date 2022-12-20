5d5a6307fe11f0a587a3d02ac9f876abab4bbcdc
SamAdebisi <samadebisi12@gmail.com>
2022-12-20 19:02:50 +0100

    user setup

--- fashion/urls.py
+++ fashion/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 
     # Local apps
+    path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
 ]
