239cfd4c94c894bbe32b7ce63c3dfd7d041127f6
SamAdebisi <samadebisi12@gmail.com>
2022-12-24 23:37:30 +0100

    email service

--- fashion/settings.py
+++ fashion/settings.py
@@ -174,9 +174,6 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-# EMAIL_BACKENDS = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKENDS = 'django.core.mail.backends.smtp.EmailBackend'
-
 ACCOUNT_SESSION_REMEMBER = True
 
 ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
@@ -190,8 +187,12 @@ ACCOUNT_UNIQUE_EMAIL = True
 
 DEFAULT_FROM_EMAIL = 'admin@fashionstore.com'
 
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+if not DEBUG:
+    EMAIL_BACKENDS = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = os.getenv('EMAIL_HOST')
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+else:
+    EMAIL_BACKENDS = 'django.core.mail.backends.console.EmailBackend'
