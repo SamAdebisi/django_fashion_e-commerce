30582e093dcf129860b557bff6e7f98adcda6490
SamAdebisi <samadebisi12@gmail.com>
2022-12-30 15:36:47 +0100

    Search optimization

--- cloths/urls.py
+++ cloths/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ClothListView, ClothDetailView
+from .views import ClothListView, ClothDetailView, SearchResultsListView
 
 urlpatterns = [
     path('', ClothListView.as_view(), name='cloth_list'),
     path('<uuid:pk>', ClothDetailView.as_view(), name='cloth_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results'),
 ]
--- cloths/views.py
+++ cloths/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin
@@ -24,3 +25,16 @@ class ClothDetailView(
     template_name = 'cloths/cloth_detail.html'
     login_url = 'account_login'
     permission_required = 'cloths.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Cloth
+    context_object_name = 'cloth_list'
+    template_name = 'cloths/search_results.html'
+    # queryset = Cloth.objects.filter(style__icontains='senator')
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Cloth.objects.filter(
+            Q(style__icontains=query) | Q(stylist__icontains=query)
+        )
